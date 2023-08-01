b9b7e82c7e1af02ccf291d4a8059da023ac66da3
Ekta <edankhra@gmail.com>
2023-07-31 23:19:53 -0400

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,9 @@
 import React from "react";
+import NavBar from "/home/ekta/Development/code/phase-2/react-hooks-import-export-lab/src/components/NavBar.js";
+import Home from "/home/ekta/Development/code/phase-2/react-hooks-import-export-lab/src/components/Home.js";
+import About from "/home/ekta/Development/code/phase-2/react-hooks-import-export-lab/src/components/About.js";
+
+
 
 function App() {
   return (
@@ -6,8 +11,10 @@ function App() {
       <NavBar />
       <Home />
       <About />
+      
     </div>
   );
 }
 
 export default App;
+
--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,14 @@
+
+
 const username = "Liza";
 const city = "New York";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
+
+export { username, city} //from "./home/ekta/Development/code/phase-2/react-hooks-import-export-lab/src/components/Home.js";//
+export { image } //from "./home/ekta/Development/code/phase-2/react-hooks-import-export-lab/src/components/About.js";//
+
+
+
